fd4a1bcc435bbbb64e07c03af6897919796141e7
Lwillson <laura.a.willson@gmail.com>
2017-02-09 14:38:48 +0800

    link view that shows the tiny url

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,12 @@ class Link < ActiveRecord::Base
 	ALPHABET = (('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a).join
 	N = ALPHABET.length
 
+	BASE_URL = 'tinyurl-lwillson.herokuapp.com/'
+
+	def to_tiny_url
+		BASE_URL + self.encode_id
+	end
+
 	# class method
 	# gives you the id for the identifier portion of the tiny link
 	def decode_tiny_link_identifier(str)
@@ -13,7 +19,6 @@ class Link < ActiveRecord::Base
 	# returns the identifying string that is mapped from the id of the link
 	# Base N encoding
 	def encode_id
-		binding.pry
   		str = ''
 		while self.id > 0
 			str << ALPHABET[self.id.modulo(N)]
