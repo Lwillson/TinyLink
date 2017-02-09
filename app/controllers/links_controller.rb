50368cd5c6243252300fda1f2559a7537a31a4fa
Lwillson <laura.a.willson@gmail.com>
2017-02-09 11:29:18 +0800

    routes and basic controller functionality for links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,26 @@
 class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
 
-	def new
-		@link = Link.new
-	end
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(allowed_params)
+    if @link.save
+      flash[:notice] = 'Successfully created link.'
+
+      redirect_to @link
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:link).permit(:url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'links#new'
+
+  resources :links
+  #get '/links/new', to: 'links#new'
 end
